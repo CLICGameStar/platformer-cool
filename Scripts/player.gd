5619d5fff913ea53e91e4fea95837404462e9efd
Yrixou <loris.vocetorquemada@epfl.ch>
2026-02-28 14:36:21 +0100

    le cube peut se poser c tro kool

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,7 +10,8 @@ var wind_force: int = 0
 var water_force: Vector2 = Vector2.ZERO
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var crouch_shape = $crouchCollisionShape2D
+@onready var standing_shape = $NormalCollisionShape2D
 func _process(_delta: float) -> void:
 	sprite_logic()
 	
@@ -26,7 +27,13 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	elif Input.is_action_pressed("crouch"):
+		standing_shape.disabled = true
+		crouch_shape.disabled = false
+	else:
+		standing_shape.disabled = false
+		crouch_shape.disabled = true
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	horizontal_movement()
@@ -43,8 +50,12 @@ func _physics_process(delta: float) -> void:
 func sprite_logic():
 	# Orientation
 	animated_sprite.flip_h = not facing_right
-	# Idle
-	if velocity == Vector2.ZERO and is_on_floor():
+	# crouch
+	if Input.is_action_pressed("crouch"):
+		animated_sprite.play("crouch")
+		
+	#Idle
+	elif velocity == Vector2.ZERO and is_on_floor():
 		animated_sprite.play("idle")
 	# Walk
 	elif velocity.x != 0:
@@ -62,10 +73,11 @@ func horizontal_movement():
 
 func get_input_direction() -> Vector2:
 	var dir = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		dir.x += 1
-	if Input.is_action_pressed("move_left"):
-		dir.x -= 1
+	if (!Input.is_action_pressed("crouch")):
+		if Input.is_action_pressed("move_right"):
+			dir.x += 1
+		if Input.is_action_pressed("move_left"):
+			dir.x -= 1
 	return dir.normalized()
 	
 func change_wind_force(force: int):
