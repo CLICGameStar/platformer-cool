da11bc15b720cd3da54fbd08f81805e6fb0fbf34
Jiří Gebauer <jiri.gebauer.jr@gmail.com>
2026-02-28 16:36:02 +0100

    Cherries appear only in Spring

--- Scripts/cherry_tree.gd
+++ Scripts/cherry_tree.gd
@@ -4,21 +4,17 @@ const SPAWN_DELAY = 25
 
 @export var cherry_object: PackedScene
 var spawned_cherry: Node2D
-var spawn_counter: float = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	spawn_cherry()
-
+var spawn_counter: float = SPAWN_DELAY
+var spring: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if spawned_cherry == null:
+	if spawned_cherry == null and spring:
 		spawn_counter += delta
 	elif not is_instance_valid(spawned_cherry):
 		spawned_cherry = null
 	
-	if spawn_counter > SPAWN_DELAY:
+	if spawn_counter >= SPAWN_DELAY and spring:
 		spawn_counter = 0
 		spawn_cherry()
 
@@ -26,3 +22,13 @@ func spawn_cherry() -> void:
 	spawned_cherry = cherry_object.instantiate() as Node2D
 	add_child(spawned_cherry)
 	spawned_cherry.position = Vector2.ZERO
+
+func enter_spring() -> void:
+	spring = true
+
+func leave_spring() -> void:
+	if spawned_cherry:
+		spawned_cherry.queue_free()
+		spawn_counter = SPAWN_DELAY
+		spawned_cherry = null
+		spring = false
